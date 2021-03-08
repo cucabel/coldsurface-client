d10d27722c7584d6f6a0dc1dfc354ce9809e7f50
dimitrijd <dimitrijdugan@gmail.com>
2021-03-08 16:38:40 +0100

    pre-deployment

--- src/services/statistics-service.js
+++ src/services/statistics-service.js
@@ -2,7 +2,7 @@ import axios from "axios";
 class StatisticsService {
   constructor() {
     this.api = axios.create({
-      baseURL: "http://localhost:5000",
+      baseURL: process.env.REACT_APP_API_URL,
       withCredentials: true,
     });
   }
