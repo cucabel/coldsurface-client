3d6367155e9c5f0b04b5f3ca0043168390acc4f5
dimitrijd <dimitrijdugan@gmail.com>
2021-03-09 09:53:20 -0800

    added one interactive chart

--- src/pages/Dashboard/Channel.js
+++ src/pages/Dashboard/Channel.js
@@ -6,10 +6,34 @@ import Dashboard from "../../components/Dashboard/Dashboard";
 import React, { Component } from "react";
 
 class Channel extends Component {
+  state = {
+    workspace: "",
+  };
+
+  handleInput = (event) => {
+    let { value, name, type } = event.target; //  value="asdfasdf"  name="director"
+    if (type === "checkbox") {
+      value = event.target.checked;
+    }
+    this.setState({ [name]: value });
+    console.log(name, value);
+  };
+
   render() {
+    const data = this.props.statistics;
     return (
       <div>
-        <Dashboard data={this.props.statistics} />
+        <div>
+          <label htmlFor="Workspace">Channel:</label>
+          <select id="workspace" name="workspace" onChange={this.handleInput}>
+            <option value="KAGGLE">KAGGLE</option>
+            <option value="ih-bcn-web-oct2020">ih-bcn-web-oct2020</option>
+            <option value="ih-bcn-web-jan2021">ih-bcn-web-jan2021</option>
+          </select>
+          <Dashboard
+            data={data.filter((el) => el.workspace === this.state.workspace)}
+          />
+        </div>
       </div>
     );
   }
