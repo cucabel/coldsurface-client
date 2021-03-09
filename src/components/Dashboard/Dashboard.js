aae0748db3ed3eda45860e966f6ac62e83b16e19
dimitrijd <dimitrijdugan@gmail.com>
2021-03-09 03:24:41 -0800

    first graph displayed

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,11 +1,132 @@
-import React from 'react'
+import React, { PureComponent } from "react";
+import {
+  LineChart,
+  Line,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+  ResponsiveContainer,
+} from "recharts";
 
-function Dashboard() {
+const data = [
+  {
+    name: "Page A",
+    uv: 4000,
+    pv: 2400,
+    amt: 2400,
+  },
+  {
+    name: "Page B",
+    uv: 3000,
+    pv: 1398,
+    amt: 2210,
+  },
+  {
+    name: "Page C",
+    uv: 2000,
+    pv: 9800,
+    amt: 2290,
+  },
+  {
+    name: "Page D",
+    uv: 2780,
+    pv: 3908,
+    amt: 2000,
+  },
+  {
+    name: "Page E",
+    uv: 1890,
+    pv: 4800,
+    amt: 2181,
+  },
+  {
+    name: "Page F",
+    uv: 2390,
+    pv: 3800,
+    amt: 2500,
+  },
+  {
+    name: "Page G",
+    uv: 3490,
+    pv: 4300,
+    amt: 2100,
+  },
+];
+
+function Dashboard(props) {
+  const data = props.data;
+  console.log("propos in compoennet", data);
   return (
-    <div> 
-      <h1>Dashboard Page</h1>
+    <div>
+      <p>Dashboard response</p>
+      <LineChart
+        width={500}
+        height={300}
+        data={data}
+        margin={{
+          top: 5,
+          right: 30,
+          left: 20,
+          bottom: 5,
+        }}
+      >
+        <CartesianGrid strokeDasharray="3 3" />
+        <XAxis dataKey="sentiment" />
+        <YAxis />
+        <Tooltip />
+        <Legend />
+        <Line
+          type="monotone"
+          dataKey="joy"
+          stroke="#8884d8"
+          activeDot={{ r: 8 }}
+        />
+        <Line type="monotone" dataKey="Time Series" stroke="#82ca9d" />
+      </LineChart>
     </div>
-  )
+  );
 }
 
 export default Dashboard;
+
+// class Dashboard extends PureComponent {
+//   static demoUrl = "https://codesandbox.io/s/simple-line-chart-kec3v";
+
+//   render() {
+//     return (
+//       <p>
+//         Dashboard response <br /> {data[0].name}
+//       </p>
+//       // <ResponsiveContainer width="100%" height="100%">
+//       //   <LineChart
+//       //     width={500}
+//       //     height={300}
+//       //     data={data}
+//       //     margin={{
+//       //       top: 5,
+//       //       right: 30,
+//       //       left: 20,
+//       //       bottom: 5,
+//       //     }}
+//       //   >
+//       //     <CartesianGrid strokeDasharray="3 3" />
+//       //     <XAxis dataKey="name" />
+//       //     <YAxis />
+//       //     <Tooltip />
+//       //     <Legend />
+//       //     <Line
+//       //       type="monotone"
+//       //       dataKey="pv"
+//       //       stroke="#8884d8"
+//       //       activeDot={{ r: 8 }}
+//       //     />
+//       //     <Line type="monotone" dataKey="uv" stroke="#82ca9d" />
+//       //   </LineChart>
+//       // </ResponsiveContainer>
+//     );
+//   }
+// }
+
+// export default Dashboard;
--- src/pages/Dashboard/Channel.js
+++ src/pages/Dashboard/Channel.js
@@ -1,17 +1,17 @@
 // import { Link } from 'react-router-dom';
-import { statistics } from './../../context/statistics-context'
+import { statistics } from "./../../context/statistics-context";
 
-// import Dashboard from '../../components/Dashboard/Dashboard';
+import Dashboard from "../../components/Dashboard/Dashboard";
 
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 class Channel extends Component {
   render() {
     return (
       <div>
-        <p>{this.props.statistics[0].day}</p>
+        <Dashboard data={this.props.statistics} />
       </div>
-    )
+    );
   }
 }
 
